3d3a63265e91a9d7a2e013b5e17da9f717052cb6
Izaak van Dongen <izaak.van.dongen@gmail.com>
2019-06-20 20:47:45 +0100

    Fix comment in dummy_apparix

--- demo/dummy_apparish_init.sh
+++ demo/dummy_apparish_init.sh
@@ -1,10 +1,9 @@
 # create a dummy apparixrc file, clobbering it on purpose
-# this is NOT how you normally use apparix. Instead you run apparix-init one
-# time, and then, do something like
-#     $ bm "hello world"
+# this is NOT how you normally use apparix. Instead you run something like
+#     $ bm hworld
 # when you're in the directory of interest
 true > "$APPARIXEXPAND"
 cat > "$APPARIXRC" <<EOF
-j,hello world,$PWD
+j,hworld,$PWD
 EOF
-echo "set dummy bookmark 'hello world' at $PWD in $APPARIXRC"
+echo "set dummy bookmark 'hworld' at $PWD in $APPARIXRC"
