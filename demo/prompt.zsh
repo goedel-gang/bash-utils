281cf221e092d45796de92c6355933bf53274fc4
Izaak van Dongen <izaak.van.dongen@gmail.com>
2019-06-20 21:05:23 +0100

    Update prompt.zsh

--- demo/prompt.zsh
+++ demo/prompt.zsh
@@ -1,16 +1,17 @@
-# Zsh prompt with vi mode indicator, git information, and powerline-ey filled in
-# background. Known to not look terrible in gruvbox colours.
-# This prompt taken from
-# goedel-gang/dotfiles/master/.zsh/prompts
-# it's not the one I actually use. as I use powerlevel10k. I have also written a
-# custom p10k segment for apparix that can be found at the aforementioned
-# location.
+#                                          __
+# ______ _______   ____    _____  ______ _/  |_
+# \____ \\_  __ \ /  _ \  /     \ \____ \\   __\
+# |  |_> >|  | \/(  <_> )|  Y Y  \|  |_> >|  |
+# |   __/ |__|    \____/ |__|_|  /|   __/ |__|
+# |__|                         \/ |__|
+# FIGMENTIZE: prompt
 
+# alternative prompt without any plugins or fancy fonts. Basically emulates
+# the important bits of my powerline prompt
 autoload -Uz vcs_info
-precmd_vcs_info() { vcs_info }
+function precmd_vcs_info() { vcs_info }
 precmd_functions+=( precmd_vcs_info )
 setopt prompt_subst
-setopt transient_rprompt
 # RPROMPT=\$vcs_info_msg_0_
 # PROMPT=\$vcs_info_msg_0_'%# '
 # basically ripped from man zshcontrib
@@ -18,9 +19,9 @@ setopt transient_rprompt
 # need to use %%b for bold off
 # TODO: customise this for dirty state
 zstyle ':vcs_info:*' actionformats \
-    '%K{green}%F{black} (%s)-[%b|%a]%u%c %f%k'
+    '%K{002}%F{000} (%s)-[%b|%a]%u%c %f%k'
 zstyle ':vcs_info:*' formats       \
-    '%K{green}%F{black} (%s)-[%b]%u%c %f%k'
+    '%K{002}%F{000} (%s)-[%b]%u%c %f%k'
 zstyle ':vcs_info:*' stagedstr "*"
 zstyle ':vcs_info:*' unstagedstr "+"
 zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b:%r'
@@ -29,22 +30,22 @@ zstyle ':vcs_info:*' enable git cvs svn hg
 zstyle ':vcs_info:*' check-for-changes true
 
 # right prompt with some information
-status_prompt="%F{black}%(?.%F{green}OK .%K{red}%B%F{yellow} %? %b)"
-shlvl_prompt="%(2L.%F{black}%K{yellow} %L .)"
-hist_prompt="%K{blue}%F{black} %h %k%f"
+status_prompt="%F{000}%(?.%F{002}OK .%K{001}%B%F{003} %? %b)"
+shlvl_prompt="%(2L.%F{000}%K{003} %L .)"
+hist_prompt="%K{004}%F{000} %h %k%f"
 RPROMPT="$status_prompt$shlvl_prompt$hist_prompt"
 
 function zle-line-init zle-keymap-select {
     # notify-send "$(date +%M:%S) zle $KEYMAP"
     case "$KEYMAP" in
         main|viins)
-            vi_colour=cyan
+            vi_colour=006
             ;;
         vicmd)
-            vi_colour=magenta
+            vi_colour=005
             ;;
         *)
-            vi_colour=white
+            vi_colour=015
             ;;
     esac
     zle reset-prompt
@@ -67,8 +68,8 @@ if [[ "$GOEDEL_APPARIX" == "true" ]]; then
 else
     apparix_indicator=""
 fi
-host_prompt="%(!.%F{yellow}%K{red}.%F{black}%K{yellow}) %n@%m "
-dir_prompt="%F{black}%K{blue} %~ "
+host_prompt="%(!.%F{003}%K{001}.%F{000}%K{003}) %n@%m "
+dir_prompt="%F{000}%K{004} %~ "
 PROMPT=$'\n'"%F{\$vi_colour}┌─$host_prompt$dir_prompt$apparix_indicator%k\$vcs_info_msg_0_"$'\n%F{\$vi_colour}└─%f '
 PROMPT2=".. "
-RPROMPT2="%_"
+RPROMPT2="%K{cyan} %^ %k"
